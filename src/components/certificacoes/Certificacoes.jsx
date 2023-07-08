9d09021ccb38206c60fb22808d166d63a043fd4a
GabrielSaints7 <DevelopergabrielSaints@gmail.com>
2023-07-08 13:15:48 -0400

    nem version 1.0.3

--- src/components/certificacoes/Certificacoes.jsx
+++ src/components/certificacoes/Certificacoes.jsx
@@ -4,48 +4,53 @@ import IMGs from '../../assets/portfolio6.jpg'
 
 
 const Certificacoes = () => {
+
+  let certificados = [
+    {
+      image: IMGs,
+      titulo: 'Certificado',
+      acesso: 'https://www.google.com'
+    },
+    {
+      image: IMGs,
+      titulo: 'Certificado2',
+      acesso: 'https://www.google.com'
+    },
+    {
+      image: IMGs,
+      titulo: 'Certificado3',
+      acesso: 'https://www.google.com'
+    },
+    {
+      image: IMGs,
+      titulo: 'Certificado4',
+      acesso: 'https://www.google.com'
+    },
+  ]
+
+
   return (
     <section id="certificacoes">
       <h5>Comprovação de conhecimentos</h5>
       <h2>Certificações</h2>
 
       <div className="container certificacoes__container">
-        <article className="certificacoes__item">
-          <div className="certificacoes__item-img">
-            <img src={IMGs} alt="" />
-          </div>
-          <h3>Titulo do certificado</h3>
-          <div className="certificacoes-cta">
-            <a href="www.google.com" className="btn btn-primary">acessar</a>
-          </div>
-        </article>
-        <article className="certificacoes__item">
-          <div className="certificacoes__item-img">
-            <img src={IMGs} alt="" />
-          </div>
-          <h3>Titulo do certificado</h3>
-          <div className="certificacoes-cta">
-            <a href="www.google.com" className="btn btn-primary">acessar</a>
-          </div>
-        </article>
-        <article className="certificacoes__item">
-          <div className="certificacoes__item-img">
-            <img src={IMGs} alt="" />
-          </div>
-          <h3>Titulo do certificado</h3>
-          <div className="certificacoes-cta">
-            <a href="www.google.com" className="btn btn-primary">acessar</a>
-          </div>
-        </article>
-        <article className="certificacoes__item">
-          <div className="certificacoes__item-img">
-            <img src={IMGs} alt="" />
-          </div>
-          <h3>Titulo do certificado</h3>
-          <div className="certificacoes-cta">
-            <a href="www.google.com" className="btn btn-primary">acessar</a>
-          </div>
-        </article>
+        {certificados.map(({image, titulo,acesso}) => {
+          return (
+            <article className="certificacoes__item">
+              <div className="certificacoes__item-img">
+                <img src={image} alt="" />
+              </div>
+              <h3>{titulo}</h3>
+              <div className="certificacoes-cta">
+                <a href={acesso} className="btn btn-primary">acessar</a>
+              </div>
+            </article>
+          )
+
+        })}
+
+
       </div>
     </section>
   );
